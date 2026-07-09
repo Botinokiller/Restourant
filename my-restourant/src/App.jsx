023cf8c1a7195068599f3e67eb8cedb00a028a73
Marucus_1 <JJMDJ2002@gmail.com>
2026-07-10 00:53:46 +0500

    Commit

--- my-restourant/src/App.jsx
+++ my-restourant/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Navbar from './Components/navbar.jsx';
-import Line from './assets/icons/navbar__line.svg'
+import Navbar from './Components/navbar'
+import line from './assets/icons/navbar__line.svg'
 
 function App() {
 
   return (
     <>
-    <Navbar/>
+    <Navbar line={line}/>
 
 
     </>
